e49c5b7f150672b7a025863d402dbe7899319c4f
rerejii <hayakawa.shinya.kochi@gmail.com>
2019-08-05 22:59:10 +0900

    '掛け算割り算データ転送'

--- EX.v
+++ EX.v
@@ -10,17 +10,39 @@ module EX(
 	output [31:0] newPC
 );
 `include "common_param.vh"
-reg [31:0] HIreg;
-reg [31:0] LOreg;
 parameter ONE32 = 32'h1;
 parameter ZERO32 = 32'h0;
-assign {Result, newPC} = ALU(Ins, Rdata1, Rdata2, Ed32, nextPC);
+parameter BLTZ_OR_BGEZ = BLTZ;
+reg [31:0] HIreg = ZERO32;
+reg [31:0] LOreg = ZERO32;
+
+assign {Result, newPC} = ALU(Ins, Rdata1, Rdata2, Ed32, nextPC, HIreg, LOreg);
 
 always @(posedge CLK)
 	begin
-		HIreg <= 0;
-		LOreg <= 0;
+		{HIreg, LOreg} <= MULT_DIV(Ins, Rdata1, Rdata2, HIreg, LOreg);
 	end
+
+// ----- MULT_DIV -------------------------------------------
+function [63:0] MULT_DIV(
+	input [31:0] Ins,
+	input [31:0] Rdata1,
+	input [31:0] Rdata2,
+	input [31:0] HIreg,
+	input [31:0] LOreg
+);
+	if(Ins[31:26] == R_FORM)
+		begin
+			case(Ins[5:0])
+				MULT: MULT_DIV = Rdata1 * Rdata2;
+				MULTU: MULT_DIV = Rdata1 * Rdata2;
+				DIV: MULT_DIV = Rdata1 / Rdata2;
+				DIVU: MULT_DIV = Rdata1 / Rdata2;
+				default: MULT_DIV = {HIreg, LOreg};
+			endcase
+		end
+	else MULT_DIV = {HIreg, LOreg};
+endfunction // MULT_DIV
 
 // ----- ALU -------------------------------------------
 function [63:0] ALU(
@@ -28,13 +50,15 @@ function [63:0] ALU(
 	input [31:0] Rdata1,
 	input [31:0] Rdata2,
 	input [31:0] Ed32,
-	input [31:0] nextPC
+	input [31:0] nextPC,
+	input [31:0] HIreg,
+	input [31:0] LOreg
 );
 	case(Ins[31:26])
-		R_FORM: ALU = R_FUNC(Ins, Rdata1, Rdata2, Ed32, nextPC);
+		R_FORM: ALU = R_FUNC(Ins, Rdata1, Rdata2, Ed32, nextPC, HIreg, LOreg);
 		J: ALU = J_FUNC(Ins, nextPC);
 		JAL: ALU = J_FUNC(Ins, nextPC);
-		default: ALU = I_FUNC(Ins, Rdata1, Ed32, nextPC);
+		default: ALU = I_FUNC(Ins, Rdata1, Rdata2, Ed32, nextPC);
 	endcase
 endfunction // ALU
 
@@ -44,7 +68,9 @@ function [63:0] R_FUNC(
 	input [31:0] Rdata1,
 	input [31:0] Rdata2,
 	input [31:0] Ed32,
-	input [31:0] nextPC
+	input [31:0] nextPC,
+	input [31:0] HIreg,
+	input [31:0] LOreg
 );
 	case(Ins[5:0])
 		// ===== ALU =====
@@ -77,8 +103,17 @@ function [63:0] R_FUNC(
 		SRLV: R_FUNC = {(Rdata1 >> Rdata2), nextPC};
 		SRAV: R_FUNC = {(Rdata1 >>> Rdata2), nextPC};
 		// ===== Jump =====
-		JR: R_FUNC = {ZERO32, Rdata1};
+		JR: R_FUNC = {ZERO32, Rdata1}; // Rdata1の値に飛ぶ
 		JALR: R_FUNC = {nextPC, Rdata1}; // 本来のPCをレジスタに退避
+		// ===== Move ====
+		MFHI: R_FUNC = {HIreg, Rdata1};  // HIregの値を返す
+		MTHI: R_FUNC = {ZERO32, nextPC}; // HIregの値を更新(こっちでは何もしない)
+		MFLO: R_FUNC = {LOreg, Rdata1};  // LOregの値を返す
+		MTLO: R_FUNC = {ZERO32, nextPC}; // LOregの値を更新(こっちでは何もしない)
+		MULT: R_FUNC = {ZERO32, nextPC}; // 乗算(こっちでは何もしない)
+		MULTU: R_FUNC = {ZERO32, nextPC}; // 乗算(こっちでは何もしない)
+		DIV: R_FUNC = {ZERO32, nextPC}; // 除算(こっちでは何もしない)
+		DIVU: R_FUNC = {ZERO32, nextPC}; // 除算(こっちでは何もしない)
 		// ===== default =====
 		default: R_FUNC = {ZERO32, nextPC};
 	endcase
@@ -88,6 +123,7 @@ endfunction // R_FUNC
 function [63:0] I_FUNC(
 	input [31:0] Ins,
 	input [31:0] Rdata1,
+	input [31:0] Rdata2,
 	input [31:0] Ed32,
 	input [31:0] nextPC
 );
@@ -104,10 +140,25 @@ function [63:0] I_FUNC(
 		ORI: I_FUNC = {Rdata1 | Ed32, nextPC};
 		XORI: I_FUNC = {Rdata1 ^ Ed32, nextPC};
 		// ===== load =====
-		LW: I_FUNC = {Rdata1 + Ed32, nextPC}; //未実装
-		SW: I_FUNC = {Rdata1 + Ed32, nextPC}; //未実装
+		LW: I_FUNC = {Rdata1 + Ed32, nextPC}; //ロードする座標を返す
+		SW: I_FUNC = {Rdata1 + Ed32, nextPC}; //ストアする座標を返す
 		// ===== Branch =====
-		BLTZ: I_FUNC = {ZERO32, nextPC}; //(BGEZも然り？)
+		//BLTZ: I_FUNC = {ZERO32, nextPC}; //(BGEZも然り？)
+		BLTZ_OR_BGEZ:
+			begin
+				case(Ins[20:16])
+					BLTZ_r:
+						begin
+							if(Rdata1 < 0) I_FUNC = {ONE32, nextPC};
+							else I_FUNC = {ZERO32, nextPC};
+						end
+					BGEZ_r:
+					begin
+						if(Rdata1 > 0) I_FUNC = {ONE32, nextPC};
+						else I_FUNC = {ZERO32, nextPC};
+					end
+				endcase
+			end
 		BEQ: I_FUNC = {ZERO32, nextPC};
 		BNE: I_FUNC = {ZERO32, nextPC};
 		BLEZ: I_FUNC = {ZERO32, nextPC};
@@ -126,7 +177,7 @@ function [63:0] J_FUNC(
 	case(Ins[31:26])
 		// ===== Jump =====
 		J: J_FUNC = { ZERO32, {nextPC[31:28], (Ins[25:0] << 2)} };
-		JAL: J_FUNC = { ZERO32, {nextPC[31:28], (Ins[25:0] << 2)} }; //戻りアドレスが$raレジスタにロードされる
+		JAL: J_FUNC = { nextPC, {nextPC[31:28], (Ins[25:0] << 2)} }; //戻りアドレスが$raレジスタにロードされる
 		// ===== default =====
 		default: J_FUNC = {ZERO32, nextPC};
 	endcase
